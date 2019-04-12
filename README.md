460d8e56a6629fcd4186548b014bbb6630efb862
hkhv <746392783@qq.com>
2019-04-12 11:28:08 +0800

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Development
+
+1. Run `npm install` to install dependencies.
+2. Run `npm run start` for local development.
+3. Run `npm run build` to build 
+
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+
+
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+module.exports = {
+    mode: "production",
+    entry: {
+        "index": "./src/ts/index.ts"
+    },
+    devtool: false,
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                    },
+                    'sass-loader?sourceMap'
+                ]
+            },
+            {
+                test: /.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets' },
+        ]),
+        new HtmlWebpackPlugin({
+            template: 'src/sites/cn/index.html',
+            inject: 'body',
+            xhtml: true,
+            metadata: {
+                isDevServer: false
+            },
+            minify: {
+              caseSensitive: true,
+              collapseWhitespace: true,
+              keepClosingSlash: true
+            },
+            chunks: 'all',
+            filename: 'index.html'
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[chunkhash].bundle.js',
+        sourceMapFilename: '[file].map',
+        chunkFilename: '[name].[chunkhash].chunk.js'
+    }
+}
